d34c4855973cb31fc7fc8408258199e2751cae0e
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-17 17:16:33 +0530

    Add-Ability to analyse Mood using Parameterized constructor

--- MoodAnalyser/MoodAnalyzerTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyzerTest/UnitTest1.cs
@@ -6,6 +6,9 @@ namespace MoodAnalyzerTest
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Uc1-TC1.1--Should return Sad
+        /// </summary>
         [TestMethod]
         public void GivingSadMessage_ShouldReturnSad()
         {
@@ -13,6 +16,9 @@ namespace MoodAnalyzerTest
             string result = message.msg1;
             Assert.AreEqual("I am in Sad Mood", result);
         }
+        /// <summary>
+        /// Uc1-TC1.2-Should return Happy
+        /// </summary>
         [TestMethod]
         public void GivingHappyMessage_ShouldReturnHappy()
         {
